1bfd6a764adcec522daaa60c78f84bd2f4cc8db8
Michele Adriaans <mcadriaans@gmail.com>
2026-04-08 12:19:34 +0200

    bronze_bookings model update

--- airbnb_dbt/models/bronze/bronze_bookings.sql
+++ airbnb_dbt/models/bronze/bronze_bookings.sql
@@ -1,13 +1,29 @@
 {{ config(
     materialized='incremental',
-    unique_key='BOOKING_ID',
+    unique_key='booking_id',
     on_schema_change='sync_all_columns'
 ) }}
 
+WITH source_data AS (
+    SELECT
+        booking_id,
+        listing_id,
+        CAST(booking_date AS timestamp_ntz) AS booking_date,
+        nights_booked,
+        cleaning_fee,
+        service_fee,
+        lower(trim(booking_status)) AS booking_status,
+        CAST(created_at AS timestamp_ntz) AS created_at,
+        current_timestamp() AS ingested_at
+    FROM {{ source('staging', 'bookings') }}
+)
+
 SELECT *
-FROM {{ source('staging', 'bookings') }}
+FROM source_data
 
 {% if is_incremental() %}
-  WHERE CREATED_AT > (SELECT MAX(CREATED_AT) FROM {{ this }}
-  )
+    WHERE created_at > (SELECT MAX(created_at) FROM {{ this }})
 {% endif %}
+
+
+

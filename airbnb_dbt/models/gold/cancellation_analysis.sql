29048b5dfc31901956561b35fdc0a267676a9416
Michele Adriaans <mcadriaans@gmail.com>
2026-04-14 16:30:31 +0200

    daily update

--- airbnb_dbt/models/gold/cancellation_analysis.sql
+++ airbnb_dbt/models/gold/cancellation_analysis.sql
@@ -2,15 +2,30 @@ SELECT
     country,
     city,
     property_type,
+
     COUNT(*) AS total_bookings,
+
     SUM(
         CASE
             WHEN booking_status = 'cancelled' THEN 1 
             ELSE 0 
         END 
     ) AS cancelled_bookings,
-    SUM(cancellation_fee) AS total_cancellation_fees,
-    AVG(lead_time_days) AS avg_lead_time
+    ROUND(
+        AVG(CASE WHEN booking_status = 'cancelled' THEN lead_time_days END), 
+    1) AS avg_lead_time_cancelled,
+
+    ROUND(
+        AVG(CASE WHEN booking_status != 'cancelled' THEN lead_time_days END), 
+    1) AS avg_lead_time_completed,
+
+    SUM(cancellation_fee) AS cancellation_fee_revenue,
+
+    ROUND(
+        SUM(CASE WHEN booking_status = 'cancelled' THEN 1 ELSE 0 END) * 1.0 
+        / NULLIF(COUNT(*), 0),   -- NULLIF prevents division by zero errors
+    2)AS cancellation_rate,
+
 FROM {{ ref('fct_bookings')}}
 GROUP BY 
     country,

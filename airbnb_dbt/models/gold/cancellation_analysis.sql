6a4ae24a78122802748ed3bfb0ceba0bdb480566
Michele Adriaans <mcadriaans@gmail.com>
2026-04-23 22:50:14 +0200

    daily update

--- airbnb_dbt/models/gold/cancellation_analysis.sql
+++ airbnb_dbt/models/gold/cancellation_analysis.sql
@@ -19,8 +19,8 @@ SELECT
         AVG(CASE WHEN booking_status != 'cancelled' THEN lead_time_days END), 
     1) AS avg_lead_time_completed,
     CAST(SUM(net_revenue) AS DECIMAL(10, 2)) AS actual_revenue,
-    CAST(SUM(cancellation_fee) AS DECIMAL(10, 2)) AS cancellations_revenue,
-    CAST(SUM(revenue_loss) AS DECIMAL(10, 2)) AS revenue_losses,
+    CAST(SUM(cancellation_fee) AS DECIMAL(10, 2)) AS cancellation_fee_revenue,
+    CAST(SUM(revenue_loss) AS DECIMAL(10, 2)) AS revenue_lost,
     ROUND(
         SUM(CASE WHEN booking_status = 'cancelled' THEN 1 ELSE 0 END) * 1.0 
         / NULLIF(COUNT(*), 0),   -- NULLIF prevents division by zero errors
--- airbnb_dbt/models/silver/silver_bookings.sql
+++ airbnb_dbt/models/silver/silver_bookings.sql
@@ -5,51 +5,56 @@
     transient=true
 ) }}
 
-
 WITH source_data AS (
+
     SELECT *
     FROM {{ source('airbnb', 'bronze_bookings') }}
 
     {% if is_incremental() %}
-    -- This looks back 3 days from the current maximum date in the Silver table
-    WHERE created_at >= (
-        SELECT DATEADD(day, -3, COALESCE(MAX(created_at), '1900-01-01'::timestamp_ntz))
-        FROM {{ this }}
-    )
+      WHERE created_at >= (
+          SELECT DATEADD(
+              day, -3,
+              COALESCE(MAX(created_at), '1900-01-01'::timestamp_ntz)
+          )
+          FROM {{ this }}
+      )
     {% endif %}
 
 ),
-
 silver_cleaned AS (
+
     SELECT
         booking_id,
-        listing_id,
         CAST(booking_date AS TIMESTAMP_NTZ) AS booking_date,
-        CAST(created_at AS TIMESTAMP_NTZ) AS created_at,
-        CAST(stay_start_date AS TIMESTAMP_NTZ) AS stay_start_date,
+        LOWER(TRIM(booking_status)) AS booking_status,
+        listing_id,
         nights_booked,
         CAST(booking_amount AS DECIMAL(10,2)) AS booking_amount,
         CAST(cleaning_fee AS DECIMAL(10, 2)) AS cleaning_fee,
         CAST(service_fee AS DECIMAL(10,2)) AS service_fee,
-        LOWER(TRIM(booking_status)) AS booking_status
+        CAST(cancellation_fee AS DECIMAL(10,2)) AS cancellation_fee,
+        CAST(created_at AS TIMESTAMP_NTZ) AS created_at,
+        CAST(stay_start_date AS TIMESTAMP_NTZ) AS stay_start_date
     FROM source_data
 
 ),
 
 silver_enriched AS (
+
     SELECT
         *,
-        -- business metrics
-        CAST(      
+        CAST(
             {{ calc_total_revenue(
-             'booking_amount',
-             'cleaning_fee',
-             'service_fee'
-        )}} 
+                'booking_amount',
+                'cleaning_fee',
+                'service_fee'
+            ) }}
         AS DECIMAL(10,2)) AS total_revenue,
         DATEADD(day, nights_booked, stay_start_date) AS stay_end_date,
         DATEDIFF(day, booking_date, stay_start_date) AS lead_time_days
     FROM silver_cleaned
+
 )
 
-SELECT * FROM silver_enriched
+
+SELECT * FROM silver_enriched ;

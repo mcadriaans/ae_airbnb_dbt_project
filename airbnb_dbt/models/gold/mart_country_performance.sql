6fe4349531389456f1a5c361dc017d74f358fe7c
Michele Adriaans <mcadriaans@gmail.com>
2026-05-16 18:42:00 +0200

    update marts

--- airbnb_dbt/models/gold/mart_country_performance.sql
+++ airbnb_dbt/models/gold/mart_country_performance.sql
@@ -3,7 +3,7 @@ SELECT
     COUNT(booking_id) AS total_bookings,
     CAST(SUM(net_revenue_loss) AS DECIMAL(10, 2)) AS net_revenue_loss,
     CAST(AVG(net_revenue_loss)  AS DECIMAL(10, 2)) AS avg_booking_value,
-    ROUND(AVG(avg_host_rating), 0) AS avg_rating
+    ROUND(AVG(avg_host_rating), 2) AS avg_rating
 FROM {{ ref('fact_bookings')}}
 GROUP BY country
 ORDER BY net_revenue_loss DESC
--- airbnb_dbt/models/gold/mart_superhost_analysis.sql
+++ airbnb_dbt/models/gold/mart_superhost_analysis.sql
@@ -1,7 +1,9 @@
 SELECT
     is_superhost,
     COUNT(*) AS bookings,
-    CAST(AVG(net_revenue) AS DECIMAL(10,2)) AS avg_net_revenue,
-    ROUND(CAST(SUM(net_revenue) AS DECIMAL(10,2)) / 1000000, 2) AS total_net_revenue_millions
+    CAST(AVG(actual_revenue) AS DECIMAL(10,2)) AS avg_revenue_per_booking,
+    CAST(SUM(actual_revenue) AS DECIMAL(10,2)) AS total_revenue,
+    CAST(AVG(net_revenue_loss) AS DECIMAL(10,2)) AS avg_net_revenue_loss,
+    CAST(SUM(net_revenue_loss) AS DECIMAL(10,2)) AS total_net_revenue_loss
 FROM {{ ref('fact_bookings') }}
 GROUP BY is_superhost

5086484d26b70aa87946dc9f18a27d36c1a98fee
Michele Adriaans <mcadriaans@gmail.com>
2026-05-18 14:45:07 +0200

    project clean-up

--- airbnb_dbt/macros/calc_actual_revenue.sql
+++ airbnb_dbt/macros/calc_actual_revenue.sql
@@ -1,6 +1,6 @@
 {% macro calc_actual_revenue(booking_status, booking_total, cancellation_fee) %}
     CASE
-        WHEN LOWER({{ booking_status }}) = 'cancelled' THEN {{ cancellation_fee }}
+        WHEN booking_status = 'cancelled' THEN {{ cancellation_fee }}
         ELSE {{ booking_total }}
     END
 {% endmacro %}
--- airbnb_dbt/macros/calc_net_revenue_loss.sql
+++ airbnb_dbt/macros/calc_net_revenue_loss.sql
@@ -1,6 +1,6 @@
 {% macro calc_net_revenue_loss(booking_status, booking_revenue, cancellation_fee) %}
         CASE
-        WHEN LOWER({{ booking_status }}) = 'cancelled' 
+        WHEN booking_status = 'cancelled' 
             THEN {{ booking_revenue }} - {{ cancellation_fee }}
         ELSE 0
     END
